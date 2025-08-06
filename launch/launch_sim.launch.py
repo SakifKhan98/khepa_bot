14927467aa878b468e62cb715a0c98d3418e7a05
SakifKhan98 <sakifkhan98@gmail.com>
2025-08-06 05:42:19 -0500

    Added 4wd functionality on base design

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -50,6 +50,7 @@ def generate_launch_description():
         executable="twist_mux",
         parameters=[twist_mux_params, {"use_sim_time": True}],
         remappings=[("/cmd_vel_out", "/diff_cont/cmd_vel_unstamped")],
+        # remappings=[("/cmd_vel_out", "/diff_cont/cmd_vel")],
     )
 
     default_world = os.path.join(
